c03418dbca188d849fa28bd6edc9a52da789154a
Arthur Péchayre <art.pechayre@gmail.com>
2019-02-20 13:08:27 +0100

    Tweak movements

--- Const.h
+++ Const.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define GRAVITY 17
+#define GRAVITY 5
 #define FRICTION 0.85f
 #define PLAYER_SPEED 45
 #define PLAYER_MAX_SPEED_X 170
-#define PLAYER_MAX_SPEED_Y 170
+#define PLAYER_MAX_SPEED_Y 190
 #define PLAYER_JUMP_CD 0.5 // In seconds
 #define PLAYER_JUMP_FORCE 2000
